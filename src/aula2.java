24ae4081ea9680b178b2b41f0de965b8b9d651f5
perkles <lucasrochasilva90@hotmail.com>
2022-10-19 20:13:35 -0300

    aula01

---
+++ src/aula2.java
@@ -0,0 +1,11 @@
+public class aula2 {
+    public static void main(String[] args){
+        //para uso de uma constatnte usamos a palavra "final" mas por convenção, esta  variavel deve estar em caixa alta
+        int numero = 5;
+        numero = 10;
+        final double VALOR_DE_PI = 3.15;
+        System.out.println(numero);
+
+    
+}
+}
