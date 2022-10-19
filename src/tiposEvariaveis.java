20a6e422e15c63cd28f994d680df5ec5eb57e365
perkles <lucasrochasilva90@hotmail.com>
2022-10-19 19:59:33 -0300

    TipoEvariaveis

---
+++ src/tiposEvariaveis.java
@@ -0,0 +1,16 @@
+public class tiposEvariaveis {
+    //Forma correta de  atribuir um valor milhar em doodle.
+    public static void main(String[] args){
+        double salarioMinimo1 = 2500;
+        double salaMinimo2 = 2500.90;
+        double numero = 2.5;
+        //byte idade = 123;
+        //short ano = 2021
+        //int cep = 21 070333
+        //long cpf = 9876543210L precisa terminar com L para indicar que é um numero longo*//
+        //float pi = 3.14F
+        //double salario = 1275.90;
+    }
+
+    
+}
