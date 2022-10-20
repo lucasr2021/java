c81e0d888fa05b18d9b0d179378205f155aa82c7
perkles <lucasrochasilva90@hotmail.com>
2022-10-19 23:05:33 -0300

    CursoJavaBasico

--- src/operadorUnarioAula4.java
+++
@@ -1,9 +0,0 @@
-public class operadorUnarioAula4 {
-    public static void main(String[] args){
-        int numero = 5;
-        System.out.println(-numero);
-
-        System.out.println(numero);
-    }
-    
-}
