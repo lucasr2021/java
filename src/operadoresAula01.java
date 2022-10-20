80c4067fe8045edeaeb116b1ba2e900f247f3912
perkles <lucasrochasilva90@hotmail.com>
2022-10-19 21:14:23 -0300

    CursojavaBasico

--- src/operadoresAula01.java
+++ src/operadoresAula01.java
@@ -2,6 +2,8 @@ public class operadoresAula01 {
 
     //Forma correta de  atribuir um valor milhar em doodle.
     public static void main(String[] args){
+        String nomeCompleto = "Glayson" + "Java";
+        System.out.println(nomeCompleto);
         
         }
     }
