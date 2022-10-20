9590874d1142c3554b0aeabda00114a4433432c9
perkles <lucasrochasilva90@hotmail.com>
2022-10-20 12:06:39 -0300

    CursoJavaBasico

---
+++ operadoresAula07.java
@@ -0,0 +1,15 @@
+public class operadoresAula07 {
+    public static void main(String[] args){
+        //Operadores logicos
+       boolean condicao1 = true;
+       boolean condiao2 = false;
+       if(condicao1 && condiao2){
+        System.out.println("As duas condições são verdadeiras");
+    }
+       if(condicao1 || condiao2){
+        System.out.println("Uma das condições é verdadeira");
+       }
+       System.out.println("Fim");
+}
+}
+    
