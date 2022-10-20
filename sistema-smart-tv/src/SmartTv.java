dd08b0fd2a5c8ded9db718684c724f16e2b73f83
perkles <lucasrochasilva90@hotmail.com>
2022-10-20 15:29:51 -0300

    Smart TV

--- sistema-smart-tv/src/SmartTv.java
+++ sistema-smart-tv/src/SmartTv.java
@@ -1,6 +1,39 @@
 public class SmartTv{
+    //atributos da classe Smart TV
     boolean ligado = false;
     int canal = 1;
-    int volume = 9;
+    int volume = 25;
+    //métodos de manipulação do estado da classe smart TV
+    /*****************************************************/
+     //Metodo mudar canal na Smart tv
+     public void mudarCanal(int novoCanal){
+        canal=novoCanal;
+     }
+     public void aumentarCanal(){
+        //canal =canal + 1;
+        canal ++;
+    }
+    public void dimunuirCanal(){
+        //canal =canal - 1;
+        canal --;
+    }
+    /*****************************************************/
+    //Metodo aumentar volume da Smart tv
+    public void aumentarVolume(){
+        //volume =volume + 1;
+        volume ++;
+    }
+    public void dimunuirVolume(){
+        //volume =volume - 1;
+        volume --;
+    }
+     /*****************************************************/
+    //Método ligar e desligar Smart TV
+    public void ligar(){
+        ligado = true;
+    }
+    public void desligar(){
+        ligado = false;
+    }
 
 }
--- sistema-smart-tv/src/Usuario.java
+++ sistema-smart-tv/src/Usuario.java
@@ -1,9 +1,30 @@
 public class Usuario {
     public static void main(String[] args) throws Exception {
        SmartTv smartTv = new SmartTv();
+
+       smartTv.dimunuirVolume();
+       smartTv.dimunuirVolume();
+       smartTv.dimunuirVolume();
+       smartTv.aumentarVolume();;
+
+       System.out.println("Volume atual "+smartTv.volume);
+
+       smartTv.mudarCanal(13);
+       System.out.println("Canal atual?: "+smartTv.canal);
+
+
+
+
        System.out.println("Tv ligada? "+smartTv.ligado);
        System.out.println("Canal atual?: "+smartTv.canal);
        System.out.println("Volume atual?: "+smartTv.volume);
+
+       smartTv.ligar();
+       System.out.println("Novo estatus - Tv ligada? "+smartTv.ligado);
+
+       smartTv.desligar();
+       System.out.println("Novo estatus - Tv ligada? "+smartTv.ligado);
+
 
     }
 }
