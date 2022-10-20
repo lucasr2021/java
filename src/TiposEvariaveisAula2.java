173005d4e80860a7d16c935c6053ab6add6d6ad4
perkles <lucasrochasilva90@hotmail.com>
2022-10-20 00:36:55 -0300

    CursoJavaBasico

--- src/TiposEvariaveisAula2.java
+++ src/TiposEvariaveisAula2.java
@@ -1,4 +1,4 @@
-public class aula2 {
+public class TiposEvariaveisAula2 {
     public static void main(String[] args){
         //para uso de uma constatnte usamos a palavra "final" mas por convenção, esta  variavel deve estar em caixa alta
         int numero = 5;
---
+++ src/operadoresAula05.java
@@ -0,0 +1,10 @@
+public class operadoresAula05 {
+    public static void main(String[] args){
+        int numero = 2;
+        numero = numero + 2;
+        numero = numero ++;
+        System.out.println(numero);
+        System.out.println(numero);
+        System.out.println(++ numero);
+}
+}
