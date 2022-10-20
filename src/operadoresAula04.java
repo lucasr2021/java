0998c67f299f7b9aa0dc9c71ce7f9de3681f4db1
perkles <lucasrochasilva90@hotmail.com>
2022-10-20 00:50:58 -0300

    CursoJavaBasico

---
+++ src/operadoresAula04.java
@@ -0,0 +1,11 @@
+public class operadoresAula04 {
+    public static void main(String[] args){
+        //repetição, infremenando de dois em dois
+        int numero = 2;
+        numero = numero + 2;
+        numero = numero ++;
+        System.out.println(numero);
+        System.out.println(numero);
+        System.out.println(++ numero);
+}
+}
