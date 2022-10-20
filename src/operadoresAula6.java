f5195f5d653775e2ed7e9fc65e1ba3f08e7ef59e
perkles <lucasrochasilva90@hotmail.com>
2022-10-20 01:00:34 -0300

    CursoJavaBasico

--- src/operadoresAula6.java
+++
@@ -1,9 +0,0 @@
-public class operadoresAula6 {
-    public static void main(String[] args){
-        int numero1 = 5;
-        int numero2 = 10;
-        boolean simNao = numero1 == numero2;
-        System.out.println("Numero 1 é igual numero 2? "+simNao);
-    
-}
-}
