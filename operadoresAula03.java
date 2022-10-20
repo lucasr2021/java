d4828f3b8b3b8dd6ccbd6deeb6da00d55c69c7eb
Lucas da Rocha Silva <115570591+lucasr2021@users.noreply.github.com>
2022-10-20 17:35:53 -0300

    Delete operadoresAula03.java

--- operadoresAula03.java
+++
@@ -1,9 +0,0 @@
-public class operadoresAula03 {
-    public static void main(String[] args){
-        int numero = 5;
-       numero = -numero;
-
-        System.out.println(numero);
-    }
-    
-}
