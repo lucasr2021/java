c47519c12dac46ff4ec3e262d897b5eb6223cc6b
perkles <lucasrochasilva90@hotmail.com>
2022-10-19 21:42:25 -0300

    CursoJavaBasico

---
+++ src/OperadoresAula02.java
@@ -0,0 +1,15 @@
+public class OperadoresAula02 {
+    public static void main(String[] args){
+        //Wual o resultado da expressão abaixo?
+        String concatenação = "?";
+        concatenação = 1+1+1+"1";
+        concatenação = 1+"1"+1+1;
+        concatenação = 1+"1"+1+"1";
+        concatenação = 1+"1"+1+"1";
+        concatenação = "1"+(1+1+1);
+        
+        System.out.println(concatenação);
+        
+    }
+    
+}
